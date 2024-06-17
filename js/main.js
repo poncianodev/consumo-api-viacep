7525ef90dc9760acbe9ccc35fcd5e0ecf0c33724
poncianodev <lucasbponciano@hotmail.com>
2024-06-16 23:41:34 -0300

    Foi consumida a API ViaCEP para mostrar o endereço

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
-document.querySelector(".btn-sumit input").addEventListener("click", () => {
-  alert("clicou!");
+document.addEventListener("DOMContentLoaded", () => {
+  const API_URL = `https://viacep.com.br/ws/`;
+
+  const cepInput = document.querySelector("#cep");
+  const ruaInput = document.querySelector("#rua");
+  const complementoInput = document.querySelector("#complemento");
+  const bairroInput = document.querySelector("#bairro");
+  const cidadeInput = document.querySelector("#cidade");
+  const estadoInput = document.querySelector("#uf");
+
+  async function fetchCep(cep) {
+    const response = await fetch(`${API_URL}${cep}/json/`);
+    const data = await response.json();
+    return data;
+  }
+
+  function displayAddress(address) {
+    ruaInput.value = address.logradouro;
+    complementoInput.value = address.complemento;
+    bairroInput.value = address.bairro;
+    cidadeInput.value = address.localidade;
+    estadoInput.value = address.uf;
+  }
+
+  cepInput.addEventListener("blur", async () => {
+    const cep = cepInput.value.trim().replace("-", "");
+    if (cep.length === 8) {
+      try {
+        const address = await fetchCep(cep);
+        if (!address.erro) {
+          displayAddress(address);
+        } else {
+          alert("CEP não encontrado!");
+        }
+      } catch (error) {
+        alert("Erro ao buscar o CEP!");
+      }
+    } else {
+      alert("CEP inválido!");
+    }
+  });
 });
